4727853f5da4bda39ea8269608a0fd82d1626b60
ilya57061 <ilyad509@gmail.com>
2023-01-20 21:26:57 +0300

    add edit pages

---
+++ AccountingPolessUp/Views/Administration/EditPages/PageEditOrganization.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountingPolessUp.Views.Administration.EditPages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageEditOrganization.xaml
+    /// </summary>
+    public partial class PageEditOrganization : Page
+    {
+        public PageEditOrganization()
+        {
+            InitializeComponent();
+        }
+        private void ButtonSaveEdit_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
