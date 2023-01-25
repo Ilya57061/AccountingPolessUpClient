76c8d633a4994a1fdb14f787faa289e56ca83804
Stanislavych <stasik.bartosh@mail.ru>
2023-01-25 21:43:14 +0300

    Add Page ScheduleOfCourses

---
+++ AccountingPolessUp/Views/Administration/EditPages/PageEditScheduleOfClasses.xaml.cs
@@ -0,0 +1,70 @@
+﻿using AccountingPolessUp.Implementations;
+using AccountingPolessUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountingPolessUp.Views.Administration.EditPages
+{
+    /// <summary>
+    /// Interaction logic for PageEditScheduleOfClasses.xaml
+    /// </summary>
+    public partial class PageEditScheduleOfClasses : Page
+    {
+        ScheduleOfClassesService _scheduleService = new ScheduleOfClassesService();
+        TrainingCoursesService _trainingCoursesService = new TrainingCoursesService();
+        List<TrainingCourses> _trainingCourses;
+        ScheduleOfСlasses _schedule;
+        public PageEditScheduleOfClasses(ScheduleOfСlasses schedule)
+        {
+            InitializeComponent();
+            ButtonSaveEdit.Visibility = Visibility.Visible;
+            ButtonAdd.Visibility = Visibility.Hidden;
+            _trainingCourses = _trainingCoursesService.Get();
+            DataContext = schedule;
+            _schedule = schedule;
+            BoxTrainingCourses.ItemsSource = _trainingCourses;
+            BoxTrainingCourses.SelectedIndex = _trainingCourses.IndexOf(_trainingCourses.FirstOrDefault(r => r.Id == schedule.TrainingCoursesId));
+        }
+        public PageEditScheduleOfClasses()
+        {
+            InitializeComponent();
+            ButtonSaveEdit.Visibility = Visibility.Hidden;
+            ButtonAdd.Visibility = Visibility.Visible;
+            _schedule = new ScheduleOfСlasses();
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ButtonSaveEdit_Click(object sender, RoutedEventArgs e)
+        {
+            WriteData();
+            _scheduleService.Update(_schedule);
+        }
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            WriteData();
+            _scheduleService.Create(_schedule);
+        }
+        private void WriteData()
+        {
+            _schedule.Description = Description.Text;
+            _schedule.DateStart = DateTime.Parse(DateStart.Text);
+            _schedule.DateEnd = DateTime.Parse(DateEnd.Text);
+            _schedule.WorkSpaceLink = WorkSpaceLink.Text;
+            _schedule.TrainingCoursesId = _trainingCourses.FirstOrDefault(i => i == BoxTrainingCourses.SelectedItem).Id;
+        }
+    }
+}
