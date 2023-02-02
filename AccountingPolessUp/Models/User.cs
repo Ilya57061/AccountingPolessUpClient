4bff2659ddcaa3eb8fdd3bc4bdd05aa14076658b
ilya57061 <ilyad509@gmail.com>
2023-02-02 22:27:02 +0300

    update user model

--- AccountingPolessUp/Models/User.cs
+++ AccountingPolessUp/Models/User.cs
@@ -3,10 +3,12 @@ namespace AccountingPolessUp.Models
 {
     public class User
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Login { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public bool IsAdmin { get; set; }
-        public bool isGlobalPM { get; set; } 
+        public bool isGlobalPM { get; set; }
+
     }
 }
