4f6ba7599d56d75cd3b492d8bd485ae3d74e03fa
ilya57061 <ilyad509@gmail.com>
2023-02-03 18:57:41 +0300

    edit user

--- AccountingPolessUp/Implementations/UserService.cs
+++ AccountingPolessUp/Implementations/UserService.cs
@@ -55,6 +55,7 @@ namespace AccountingPolessUp.Implementations
         {
             using (WebClient web = new WebClient())
             {
+                web.Headers.Add("Authorization", "Bearer " + TokenManager.AccessToken);
                 System.Collections.Specialized.NameValueCollection reqparm = new System.Collections.Specialized.NameValueCollection();
                 reqparm.Add("Login", $"{model.Login}");
                 reqparm.Add("Password", $"{model.Password}");
