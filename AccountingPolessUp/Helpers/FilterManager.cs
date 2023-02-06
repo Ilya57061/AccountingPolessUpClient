7d20eb2ed486edcb29eec81e6ec04875598b55d3
ilya57061 <ilyad509@gmail.com>
2023-02-06 23:46:12 +0300

    add action filters

--- AccountingPolessUp/Helpers/FilterManager.cs
+++ AccountingPolessUp/Helpers/FilterManager.cs
@@ -11,6 +11,135 @@ namespace AccountingPolessUp.Helpers
 {
     public static class FilterManager
     {
+        public static void ConfirmFilter(DataGrid dataGrid, IEnumerable<Project> list, string customer, string status, string dateStart, 
+            string dateEnd, string description, string technicalSpecification, int idLocalPM)
+        {
+         
+            if (!string.IsNullOrEmpty(description))
+            {
+                list = list.Where(x => x.Description == description);
+            }
+            if (!string.IsNullOrEmpty(customer))
+            {
+                list = list.Where(x => x.Customer.FullName == customer);
+            }
+            if (!string.IsNullOrEmpty(status))
+            {
+                list = list.Where(x => x.Status== status);
+            }
+            if (!string.IsNullOrEmpty(technicalSpecification))
+            {
+                list = list.Where(x => x.TechnicalSpecification == technicalSpecification);
+            }
+            if (idLocalPM!=null)
+            {
+                list = list.Where(x => x.idLocalPM == idLocalPM);
+            }
+            if (!string.IsNullOrEmpty(dateStart))
+            {
+                list = list.Where(x => x.DateStart == DateTime.Parse(dateStart));
+            }
+            if (!string.IsNullOrEmpty(dateEnd))
+            {
+                list = list.Where(x => x.DateEnd == DateTime.Parse(dateEnd));
+            }
+
+            dataGrid.ItemsSource = null;
+            dataGrid.Items.Clear();
+            dataGrid.ItemsSource = list;
+        }
+        public static void ConfirmFilter(DataGrid dataGrid, IEnumerable<Rank> list, string name, string description, string organization, int minMmr, int maxMmr)
+        {
+            if (!string.IsNullOrEmpty(organization))
+            {
+                list = list.Where(x => x.Organizations.Fullname == organization);
+            }
+            if (!string.IsNullOrEmpty(description))
+            {
+                list = list.Where(x => x.Description == description);
+            }
+            if (!string.IsNullOrEmpty(name))
+            {
+                list = list.Where(x => x.RankName == name);
+            }
+            if (minMmr != null)
+            {
+                list = list.Where(x => x.MinMmr == minMmr);
+            }
+            if (maxMmr != null)
+            {
+                list = list.Where(x => x.MaxMmr == maxMmr);
+            }
+            dataGrid.ItemsSource = null;
+            dataGrid.Items.Clear();
+            dataGrid.ItemsSource = list;
+        }
+        public static void ConfirmFilter(DataGrid dataGrid, IEnumerable<RegistrationForCourses> list, string dateEntry, string participant, string course)
+        {
+            if (!string.IsNullOrEmpty(participant))
+            {
+                list = list.Where(x => x.Participants.Individuals.FIO == participant);
+            }
+            if (!string.IsNullOrEmpty(course))
+            {
+                list = list.Where(x => x.TrainingCourses.Name == course);
+            }
+            if (!string.IsNullOrEmpty(dateEntry))
+            {
+                list = list.Where(x => x.DateEntry == DateTime.Parse(dateEntry));
+            }
+            dataGrid.ItemsSource = null;
+            dataGrid.Items.Clear();
+            dataGrid.ItemsSource = list;
+        }
+        public static void ConfirmFilter(DataGrid dataGrid, IEnumerable<Regulation> list, string organization, string name, string text, string description)
+        {
+            if (!string.IsNullOrEmpty(organization))
+            {
+                list = list.Where(x => x.Organization.Fullname == organization);
+            }
+            if (!string.IsNullOrEmpty(description))
+            {
+                list = list.Where(x => x.Description == description);
+            }
+            if (!string.IsNullOrEmpty(name))
+            {
+                list = list.Where(x => x.Name == name);
+            }
+            if (!string.IsNullOrEmpty(text))
+            {
+                list = list.Where(x => x.Text == text);
+            }
+            dataGrid.ItemsSource = null;
+            dataGrid.Items.Clear();
+            dataGrid.ItemsSource = list;
+        }
+        public static void ConfirmFilter(DataGrid dataGrid, IEnumerable<ScheduleOfСlasses> list, string description, string dateStart, string dateEnd, string link, string course)
+        {
+            if (!string.IsNullOrEmpty(link))
+            {
+                list = list.Where(x => x.WorkSpaceLink == link);
+            }
+            if (!string.IsNullOrEmpty(description))
+            {
+                list = list.Where(x => x.Description == description);
+            }
+            if (!string.IsNullOrEmpty(course))
+            {
+                list = list.Where(x => x.TrainingCourses.Name == course);
+            }
+            if (!string.IsNullOrEmpty(dateStart))
+            {
+                list = list.Where(x => x.DateStart == DateTime.Parse(dateStart));
+            }
+            if (!string.IsNullOrEmpty(dateEnd))
+            {
+                list = list.Where(x => x.DateEnd == DateTime.Parse(dateEnd));
+            }
+            dataGrid.ItemsSource = null;
+            dataGrid.Items.Clear();
+            dataGrid.ItemsSource = list;
+        }
         public static void ConfirmFilter(DataGrid dataGrid, IEnumerable<StagesOfProject> list, string name, string description, string dateStart, string dateEnd, string status, string project)
         {
             if (!string.IsNullOrEmpty(name))
