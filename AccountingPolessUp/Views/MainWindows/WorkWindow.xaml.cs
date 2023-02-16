89c8a4c9ef50c7d6e1a9b637292d19d98df71473
ilya57061 <ilyad509@gmail.com>
2023-02-16 23:43:33 +0300

    add switch for Button_Click

--- AccountingPolessUp/Views/MainWindows/WorkWindow.xaml.cs
+++ AccountingPolessUp/Views/MainWindows/WorkWindow.xaml.cs
@@ -2,20 +2,8 @@
 using AccountingPolessUp.Views;
 using AccountingPolessUp.Views.Administration;
 using AccountingPolessUp.Views.Information;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AccountingPolessUp
 {
@@ -48,9 +36,9 @@ namespace AccountingPolessUp
                         item.Visibility = Visibility.Collapsed;
                         MainDepartments.Visibility = Visibility.Visible;
                         Departments.Visibility = Visibility.Visible;
-                        Positions.Visibility = Visibility.Visible;
-                        Employments.Visibility = Visibility.Visible;
-                        FinalProjects.Visibility = Visibility.Visible;
+                        ButtonAdmPosition.Visibility = Visibility.Visible;
+                        ButtonAdmWork.Visibility = Visibility.Visible;
+                        ButtonAdmFinalProjects.Visibility = Visibility.Visible;
                     }
                     if (_user.Role.Name == "DirectorOrganizational")
                     {
@@ -83,144 +71,118 @@ namespace AccountingPolessUp
             authorization.Show();
             this.Close();
         }
-        private void ButtonProfile_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new PageProfile(_user);
-        }
-        private void ButtonPositions_Click(object sender, RoutedEventArgs e) // вакансии
-        {
-            MainFrame.Content = new PagePositions();
-        }
-        private void ButtonMentor_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageMentor();
-        }
-        private void ButtonEmployment_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageEmployment();
-        }
-        private void ButtonProjects_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageProjects();
-        }
-        private void ButtonInfoRules_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoRules();
-        }
-        private void ButtonInfoDepartments_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoDepartments();
-        }
-        private void ButtonInfoPositiones_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoPositions();
-        }
-        private void ButtonInfoСourses_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoСourses();
-        }
-        private void ButtonInfoBonus_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoBonus();
-        }
-        private void ButtonInfoRanks_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoRanks();
-        }
-        private void ButtonInfoPositions_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoPositions();
-        }
+            TreeViewItem button = sender as TreeViewItem;
+            switch (button.Name)
+            {
 
-        #region Adm
-        private void ButtonAdmRules_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmRules();
+                case "ButtonProfile":
+                    MainFrame.Content = new PageProfile(_user);
+                    break;
+                case "ButtonPositions":
+                    MainFrame.Content = new PagePositions();
+                    break;
+                case "ButtonMentor":
+                    MainFrame.Content = new PageMentor();
+                    break;
+                case "ButtonEmployment":
+                    MainFrame.Content = new PageEmployment();
+                    break;
+                case "ButtonProjects":
+                    MainFrame.Content = new PageProjects();
+                    break;
+                case "ButtonInfoRules":
+                    MainFrame.Content = new PageInfoRules();
+                    break;
+                case "ButtonInfoPositiones":
+                    MainFrame.Content = new PageInfoPositions();
+                    break;
+                case "ButtonInfoСourses":
+                    MainFrame.Content = new PageInfoСourses();
+                    break;
+                case "ButtonInfoBonus":
+                    MainFrame.Content = new PageInfoBonus();
+                    break;
+                case "ButtonInfoDepartments":
+                    MainFrame.Content = new PageInfoDepartments();
+                    break;
+                case "ButtonInfoRanks":
+                    MainFrame.Content = new PageInfoRanks();
+                    break;
+                case "ButtonInfoPositions":
+                    MainFrame.Content = new PageInfoPositions();
+                    break;
+                case "ButtonAdmRules":
+                    MainFrame.Content = new PageAdmRules();
+                    break;
+                case "ButtonAdmDepartments":
+                    MainFrame.Content = new PageAdmDepartments();
+                    break;
+                case "AppInTheProject":
+                    MainFrame.Content = new PageAdmAppInTheProject();
+                    break;
+                case "ButtonAdmPositiones":
+                    MainFrame.Content = new PageInfoPositions();
+                    break;
+                case "ButtonAdmCourses":
+                    MainFrame.Content = new PageAdmCourses();
+                    break;
+                case "ButtonAdmRanks":
+                    MainFrame.Content = new PageAdmRanks();
+                    break;
+                case "ButtonAdmWork":
+                    MainFrame.Content = new PageAdmWork();
+                    break;
+                case "ButtonAdmStudents":
+                    MainFrame.Content = new PageAdmStudents();
+                    break;
+                case "ButtonAdmRegistrationForCourses":
+                    MainFrame.Content = new PageAdmRegistrationForCourses();
+                    break;
+                case "StageOfProject":
+                    MainFrame.Content = new PageAdmStageOfProject();
+                    break;
+                case "ButtonAdmNatural":
+                    MainFrame.Content = new PageAdmNatural();
+                    break;
+                case "Projects":
+                    MainFrame.Content = new PageAdmProjects();
+                    break;
+                case "ButtonAdmOrganization":
+                    MainFrame.Content = new PageAdmOrganizations();
+                    break;
+                case "ButtonAdmUsers":
+                    MainFrame.Content = new PageAdmUsers();
+                    break;
+                case "ButtonAdmMembers":
+                    MainFrame.Content = new PageAdmMembers();
+                    break;
+                case "ButtonAdmBonus":
+                    MainFrame.Content = new PageAdmBonus();
+                    break;
+                case "ButtonAdmCustomer":
+                    MainFrame.Content = new PageAdmCustomer();
+                    break;
+                case "ButtonAdmEducationalPortals":
+                    MainFrame.Content = new PageAdmEducationalPortals();
+                    break;
+                case "ButtonAdmPosition":
+                    MainFrame.Content = new PageAdmPosition();
+                    break;
+                case "Departments":
+                    MainFrame.Content = new PageAdmDepartments();
+                    break;
+                case "Vacancy":
+                    MainFrame.Content = new PageAdmVacancy();
+                    break;
+                case "ButtonAdmSchedule":
+                    MainFrame.Content = new PageAdmScheduleOfClasses();
+                    break;
+                case "ButtonAdmFinalProjects":
+                    MainFrame.Content = new PageAdmFinalProject();
+                    break;
+            }
         }
-        private void ButtonAdmDepartments_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmDepartments();
-        }
-        private void ButtonAdmApplications_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmAppInTheProject();
-        }
-        private void ButtonAdmPositiones_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageInfoPositions();
-        }
-        private void ButtonAdmCourses_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmCourses();
-        }
-        private void ButtonAdmRanks_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmRanks();
-        }
-        private void ButtonAdmWork_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmWork();
-        }
-        private void ButtonAdmStudents_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmStudents();
-        }
-        private void ButtonAdmRegistrationForCourses_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmRegistrationForCourses();
-        }
-        private void ButtonAdmStageOfProject_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmStageOfProject();
-        }
-        private void ButtonAdmNatural_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmNatural();
-        }
-        private void ButtonAdmProjects_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmProjects();
-        }
-        private void ButtonAdmOrganization_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmOrganizations();
-        }
-        private void ButtonAdmUsers_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmUsers();
-        }
-        private void ButtonAdmMembers_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmMembers();
-        }
-        private void ButtonAdmBonus_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmBonus();
-        }
-        private void ButtonAdmCustomer_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmCustomer();
-        }
-        private void ButtonAdmEducationalPortals_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmEducationalPortals();
-        }
-        private void ButtonAdmPosition_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmPosition();
-        }
-        private void ButtonAdmVacancy_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmVacancy();
-        }
-        private void ButtonAdmSchedule_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmScheduleOfClasses();
-        }
-        private void ButtonAdmFinalProjects_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new PageAdmFinalProject();
-        }
-        #endregion
     }
 }
