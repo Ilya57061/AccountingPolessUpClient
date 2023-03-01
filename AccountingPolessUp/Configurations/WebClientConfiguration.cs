e0917002cae2f379236d146f33fd74018bc97eb6
Dzmitry Burak <lemif1008@yandex.ru>
2023-03-01 21:40:56 +0300

    refactoring web client configuration for all services

---
+++ AccountingPolessUp/Configurations/WebClientConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AccountingPolessUp.Models;
+using System.Net;
+using System.Text;
+
+namespace AccountingPolessUp.Configurations
+{
+    public static class WebClientConfiguration
+    {
+        public const string BaseAdress = "https://polessu.by/polessup/";
+
+        public static Encoding Encoding = Encoding.UTF8;
+
+        public static WebHeaderCollection Headers = new WebHeaderCollection
+        {
+            ["Authorization"] = "Bearer " + TokenManager.AccessToken
+        };
+    }
+}
--- AccountingPolessUp/Implementations/ParticipantsService.cs
+++ AccountingPolessUp/Implementations/ParticipantsService.cs
@@ -1,4 +1,5 @@
-﻿using AccountingPolessUp.Models;
+﻿using AccountingPolessUp.Configurations;
+using AccountingPolessUp.Models;
 using AccountingPolessUp.ViewModels;
 using Newtonsoft.Json;
 using System;
@@ -21,10 +22,10 @@ namespace AccountingPolessUp.Implementations
         {
             _webClient = new WebClient
             {
-                BaseAddress = "https://polessu.by/polessup/",
-                Headers = { ["Authorization"] = "Bearer " + TokenManager.AccessToken }
+                BaseAddress = WebClientConfiguration.BaseAdress,
+                Headers = WebClientConfiguration.Headers,
+                Encoding = WebClientConfiguration.Encoding
             };
-            _webClient.Encoding = System.Text.Encoding.UTF8;
         }
 
         public List<Participants> Get()
