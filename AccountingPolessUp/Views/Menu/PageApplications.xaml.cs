02bb0ec1ecf9cd9e90539645703f775ba52d862d
Stanislavych <stasik.bartosh@mail.ru>
2023-03-29 19:10:28 +0300

    edit PageApplications

---
+++ AccountingPolessUp/Views/Menu/PageApplications.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountingPolessUp.Views.Menu
+{
+    /// <summary>
+    /// Interaction logic for PageApplications.xaml
+    /// </summary>
+    public partial class PageApplications : Page
+    {
+        public PageApplications()
+        {
+            InitializeComponent();
+        }
+        private void HyperlinkGitHub_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+            //    Process.Start(_participants.GitHub);
+
+            //}
+            //catch (Exception)
+            //{
+            //    HyperlinkGitHub.IsEnabled = false;
+            //    InfoGitHub.Foreground = new SolidColorBrush(Colors.Gray);
+            //}
+        }
+    }
+}
